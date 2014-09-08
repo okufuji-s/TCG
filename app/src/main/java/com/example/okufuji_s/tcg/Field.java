3562112a147cc06ae5c1908cde503a9ca0c6f7dc
unknown <okufuji-s@PC-1766.ad.klab.org>
2014-09-08 19:54:04 +0900

    自分の召喚獣と補助を表す場所を仮に表示しました。

--- app/src/main/java/com/example/okufuji_s/tcg/Field.java
+++ app/src/main/java/com/example/okufuji_s/tcg/Field.java
@@ -14,23 +14,26 @@ import android.view.View;
  * Created by okufuji-s on 2014/09/08.
  */
 public class Field extends View {
+    Paint p = new Paint();
+    Bitmap back;
+    Rect backrect;
+    Rect mysummons,mysupport;
+
+
     public Field(Context context) {
         super(context);
+        Resources res = this.getContext().getResources();
+        back = BitmapFactory.decodeResource(res, R.drawable.back);
+        backrect = new Rect(0,0,back.getWidth(),back.getHeight());
+        mysummons = new Rect(480,720,600,900);
+        mysupport = new Rect(480,920,600,1100);
     }
 
     @Override
     protected void onDraw(Canvas c) {
         super.onDraw(c);
-        Paint p = new Paint();
-        Bitmap back;
-        Rect src,dst;
 
-        Resources res = this.getContext().getResources();
-        back = BitmapFactory.decodeResource(res, R.drawable.back);
-        c.drawBitmap(back,0,0,p);
-        
-        src = new Rect(0,0,back.getWidth(),back.getHeight());
-        dst = new Rect(0,0,100,150);
-        c.drawBitmap(back,src,dst,p);
+        c.drawBitmap(back,backrect,mysummons,p);
+        c.drawBitmap(back,backrect,mysupport,p);
     }
 }
