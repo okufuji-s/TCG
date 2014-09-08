6d81dd338b2bd9eb9612783e6298c5a7aaf64087
unknown <okufuji-s@PC-1766.ad.klab.org>
2014-09-08 19:36:12 +0900

    背面の画像を表示し、それを縮小して、小さくなった背面の画像を表示しました。

--- app/src/main/java/com/example/okufuji_s/tcg/Field.java
+++ app/src/main/java/com/example/okufuji_s/tcg/Field.java
@@ -1,9 +1,13 @@
 package com.example.okufuji_s.tcg;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -18,10 +22,15 @@ public class Field extends View {
     protected void onDraw(Canvas c) {
         super.onDraw(c);
         Paint p = new Paint();
-        c.drawColor(Color.GRAY);
-        p.setARGB(255,100,100,255);
-        p.setTextSize(64);
-        p.setAntiAlias(true);
-        c.drawText("hello world",50,300,p);
+        Bitmap back;
+        Rect src,dst;
+
+        Resources res = this.getContext().getResources();
+        back = BitmapFactory.decodeResource(res, R.drawable.back);
+        c.drawBitmap(back,0,0,p);
+        
+        src = new Rect(0,0,back.getWidth(),back.getHeight());
+        dst = new Rect(0,0,100,150);
+        c.drawBitmap(back,src,dst,p);
     }
 }
